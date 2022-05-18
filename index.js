65eaefa616db599ce5aeed29d9e271e88ec9e004
MahirAzeem <mahir.azeem11@gmail.com>
2022-05-18 15:43:25 +0530

    changes made

--- index.js
+++ index.js
@@ -1,13 +1,56 @@
 const express = require( "express" );
 const app = express();
 const port = 3000; // default port to listen
+const bodyparser = require('body-parser');
+app.use(bodyparser.json());
+let { productData } = require("./productData")
 
 // define a route handler for the default home page
 app.get( "/", ( req, res ) => {
-    res.send( "Hello SLIIT, my team members are Malindu, Dilan and Hussain!" );
-} );
+    res.send(productData);
+});
 
 // start the Express server
 app.listen( port, () => {
     console.log( `server started at http://localhost:${ port }` );
-} );
+});
+
+app.get("/product/:id", (req,res) => {
+    let id = +req.params.id;
+    let product = productData.find((prod) => prod.id === id);
+    if(product)
+        res.send(product);
+    else
+        res.status(404).send("No Product Found");
+})
+
+app.post("/product/add", async (req,res) => {
+    let body = await req.body;
+    console.log(body);
+    let maxid = productData.reduce((acc , curr) => (curr.id >= acc ? curr.id : acc), 0);
+    let newid = maxid + 1;
+    let newProduct = { id: newid, ...body };
+    productData.push(newProduct);
+    res.send(newProduct)
+});
+
+app.put("/product/update/:id", (req,res) => {
+    let id = +req.params.id;
+    let body = req.body;
+    let index = productData.findIndex((prod) => prod.id === id);
+    if(index >= 0) {
+        let updatedProduct = { id: id, ...body };
+        productData[index] = updatedProduct;
+        res.send(updatedProduct);   
+    }else res.status(404).send("No Product Found")
+    
+}) 
+
+app.delete("/product/delete/:id", (req, res) => {
+    let id = +req.params.id;
+    let index = productData.findIndex((prod) => prod.id === id);
+    if(index >= 0) {
+        let deletedStudent = productData.splice(index, 1);
+        res.send(deletedStudent);
+    }else res.status(404).send("No Product Found")
+})
---
+++ productData.js
@@ -0,0 +1,7 @@
+let products = [
+  { id: 1, name: "Anchor", price: 1250},
+  { id: 2, name: "Highland", price: 75},
+  { id: 3, name: "Kothmale", price: 200},
+]
+
+module.exports.productData = products;
